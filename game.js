3cdd3b462a4d569cb6e4bd236e7b50a6c94c0d9d
rikords <iamhakanduyar@gmail.com>
2023-10-30 20:53:21 +0300

    update

--- game.js
+++ game.js
@@ -1,21 +1,17 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 userClickedPattern = [];
 
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
-
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-
   var randomChosenColour = buttonColours[randomNumber];
-
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
@@ -23,7 +19,10 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+}
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 nextSequence();
