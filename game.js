3a4111c440a79ab8b29dbc4c6c866a0abff336f7
rikords <iamhakanduyar@gmail.com>
2023-11-03 00:59:26 +0300

    update

--- game.js
+++ game.js
@@ -40,6 +40,7 @@ function checkAnswer(currentLevel) {
     setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
   }
 }
 
