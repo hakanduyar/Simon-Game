655ef221fe59e3791330206e399035ad2409bd75
rikords <iamhakanduyar@gmail.com>
2023-11-02 23:45:27 +0300

    update

--- game.js
+++ game.js
@@ -26,6 +26,12 @@ $(".btn").click(function () {
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
   } else {
     console.log("wrong");
   }
